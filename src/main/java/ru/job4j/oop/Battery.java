2e6efe8b0b535301d42f4c2bff612d092969feac
Skafishka <sergey.kolygin@gmail.com>
2022-04-08 14:41:04 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public int getLoad() {
+        return load;
+    }
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        this.load += another.load;
+        another.load = this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery charger = new Battery(30);
+        Battery another = new Battery(50);
+        charger.exchange(another);
+    }
+}
