fb308a889271c55320bd33d2de9233f314093cde
SKo <sergey.kolygin@gmail.com>
2022-04-23 23:05:29 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #291728]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,10 +59,12 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         boolean rsl = index > -1;
-        System.arraycopy(items, index + 1, items, index, size - index - 1);
-        items[size - 1] = null;
-        size--;
-        return false;
+        if (rsl) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
     }
 
 }
