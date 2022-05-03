47526af5a2d1437c9c33c070d2d882dced4d7a22
SKo <sergey.kolygin@gmail.com>
2022-05-03 23:39:25 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -69,6 +69,28 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertNull(tracker.findById(0));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"7", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitProgramAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu:" + ln
+                              + "0. Exit Program" + ln
+                              + "Wrong input, you can select: 0 .. 0" + ln
+                              + "Menu:" + ln
+                              + "0. Exit Program" + ln
+                        )
+        );
+    }
 }
 
 /*
