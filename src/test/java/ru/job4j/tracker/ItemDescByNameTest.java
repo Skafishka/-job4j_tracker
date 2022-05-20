7f566d08e4fa43b6f5541230410c5939790335bc
Skafishka <sergey.kolygin@gmail.com>
2022-05-20 12:52:56 +0300

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -1,19 +1,21 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.Collections;
-import static org.hamcrest.core.Is.is;
 import java.util.List;
-import java.util.*;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class ItemAscByNameTest {
+public class ItemDescByNameTest {
 
     @Test
-    public void whenItemAscByName() {
+    public void whenItemDescByName() {
         List<Item> items = Arrays.asList(new Item("aaa"), new Item("zzz"), new Item("bbb"));
-        Collections.sort(items, new ItemAscByName());
-        assertThat(items.get(1).getName(), is("bbb"));
+        Collections.sort(items, new ItemDescByName());
+        assertThat(items.get(0).getName(), is("zzz"));
     }
 
 }
