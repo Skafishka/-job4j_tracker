7e70981a664c4d440083026e795dece494f51962
SKo <sergey.kolygin@gmail.com>
2022-05-22 23:23:32 +0300

    3. Компаратор для строк [#35008 #303339]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,31 +6,14 @@ public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int rsl = Integer.compare(left.length(), right.length());
-        if (rsl == 0) {
-            for (int i = 0; i < left.length(); i++) {
-                rsl = Character.compare(left.charAt(i), right.charAt(i));
-                if (rsl != 0) {
-                    return rsl;
-                }
+        int q = Math.min(left.length(), right.length());
+        for (int i = 0; i < q; i++) {
+            int rsl = Character.compare(left.charAt(i), right.charAt(i));
+            if (rsl != 0) {
+                return rsl;
             }
-        } else if (rsl < 0) {
-            for (int i = 0; i < left.length(); i++) {
-                rsl = Character.compare(left.charAt(i), right.charAt(i));
-                if (rsl != 0) {
-                    return rsl;
-                }
-            }
-            rsl = rsl == 0 ? -1 : 1;
-        } else {
-            for (int i = 0; i < right.length(); i++) {
-                rsl = Character.compare(left.charAt(i), right.charAt(i));
-                if (rsl != 0) {
-                    return rsl;
-                }
-            }
-            rsl = rsl == 0 ? -1 : 1;
         }
+        int rsl = Integer.compare(left.length(), right.length());
         return rsl;
     }
 
