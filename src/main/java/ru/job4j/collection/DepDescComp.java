bef14c9a25481aa887a33158c9869835ca04df1d
SKo <sergey.kolygin@gmail.com>
2022-05-24 09:29:01 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,8 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<Departments> {
+
+
+}
---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            String[] el = value.split("/", 3);
+            for (int i = 0; i < el.length - 1; i++) {
+                tmp.add(start + el[i]);
+                tmp.add(el[i] + "/" + el[i + 1]);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+    }
+
+}
