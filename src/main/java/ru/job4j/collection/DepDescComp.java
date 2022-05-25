3a042327935afc3673eac09f60c5216b846d83f5
SKo <sergey.kolygin@gmail.com>
2022-05-25 07:05:54 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 import java.util.List;
 
 public class DepDescComp implements Comparator<String> {
-    List<String> rsl = new ArrayList<>();
 
     @Override
     public int compare(String o1, String o2) {
@@ -16,18 +15,13 @@ public class DepDescComp implements Comparator<String> {
             for (int i = rslO1.length - 1; i > 0; i--) {
                 int w = rslO2[i].compareTo(rslO1[i]);
                 if (w > 0) {
-                    this.rsl.add(rslO2[i]);
                     return rslO1[i].compareTo(rslO2[i]);
                 } else if (w < 0) {
-                    this.rsl.add(rslO1[i]);
                     return rslO1[i].compareTo(rslO2[i]);
                 }
             }
         }
         return Integer.compare(rslO1.length, rslO2.length);
     }
-
-    public List<String> getRsl() {
-        return rsl;
-    }
 }
+
