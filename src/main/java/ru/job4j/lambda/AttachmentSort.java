f9ef0a67695d3935e3dba711869b0b5da7bb9f11
Skafishka <sergey.kolygin@gmail.com>
2022-06-01 10:25:05 +0300

    1.1. Анонимные классы [#214142 #305957]

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -4,13 +4,15 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+@SuppressWarnings("checkstyle:EmptyLineSeparator")
 public class AttachmentSort {
     public static void main(String[] args) {
         List<Attachment> attachments = Arrays.asList(
-                new Attachment("image 1", 100),
+                new Attachment("image 1", 10),
                 new Attachment("image 2", 34),
-                new Attachment("image 3", 13)
+                new Attachment("image 3", 25)
         );
+        System.out.println(attachments);
         Comparator<Attachment> comparatorSize =  new Comparator<>() {
             @Override
             public int compare(Attachment o1, Attachment o2) {
@@ -21,15 +23,8 @@ public class AttachmentSort {
         System.out.println(attachments);
         Comparator<Attachment> comparatorName = new Comparator<>() {
             @Override
-            public int compare(Attachment o1, Attachment o2) {
-                String[] rslO1 = o1.getName().split("");
-                String[] rslO2 = o2.getName().split("");
-                int q = Math.min(rslO2.length, rslO1.length);
-                int rsl = Integer.compare(rslO1.length, rslO2.length);
-                for (int i = 0; i < q; i++) {
-                    rsl = rslO1[i].compareTo(rslO2[i]);
-                }
-                return rsl;
+             public int compare(Attachment o1, Attachment o2) {
+                return o1.getName().compareTo(o2.getName());
             }
         };
         attachments.sort(comparatorName);
