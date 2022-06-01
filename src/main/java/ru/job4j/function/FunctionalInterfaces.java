bd58ba3c154cd206286067afeb6abd46ecc44b76
SKo <sergey.kolygin@gmail.com>
2022-06-01 23:56:25 +0300

    1.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -10,46 +10,28 @@ public class FunctionalInterfaces {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
 
-        BiConsumer<Integer, String> biCon = (s, s1) -> {
-            map.put(s, s1);
-        };
+        BiConsumer<Integer, String> biCon = (s, s1) -> map.put(s, s1);
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
 
-        /*
-            Замените вставку элементов в карту на использование BiConsumer, он уже объявлен, требуется его реализовать.
-         */
-        map.put(1, "one");
-        map.put(2, "two");
-        map.put(3, "three");
-        map.put(4, "four");
-        map.put(5, "five");
-        map.put(6, "six");
-        map.put(7, "seven");
-
-        BiPredicate<Integer, String> biPred = (i, s) ->
-        {
-            if (i % 2 == 0 || map.get(s).length() == 4) {
+        BiPredicate<Integer, String> biPred = (i, w) -> (i % 2 == 0 || w.length() == 4);
+        for (Integer i : map.keySet()) {
+            if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
-            return true;
-        };
+        }
 
-        /*
-            Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
-         */
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
-        List<String> strings = new ArrayList<>(map.values());
 
         Consumer<String> con = (s) -> System.out.println(s);
-        con.accept(sup.toString());
-
-        Function<String, String> func = ...;
-        for (String s : strings) {
-            /*
-                Заменить вывод строк на применение Consumer
-                Заменить преобразование строк к строкам в верхнем регистре с помощью Function
-                Необходимое объявлено выше, требуется их реализовать.
-            */
-            System.out.println(s.toUpperCase());
+        Function<String, String> func = (s) -> s.toUpperCase();
+        for (String s : sup.get()) {
+            con.accept(func.apply(s));
         }
     }
 }
