ba739e0bcead996521ed78d098b7da04fe249560
Skafishka <sergey.kolygin@gmail.com>
2022-06-10 15:10:06 +0300

    1. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -2,9 +2,10 @@ package ru.job4j.stream;
 
 import java.util.function.Predicate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predic) {
-        return null;
+        return students.stream().filter(predic).collect(Collectors.toList());
     }
 }
